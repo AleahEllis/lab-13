632e053e3bc3ac321cc3adb4490fac48265d015f
Aleah Ellis <aleah.is@icloud.com>
2016-02-27 18:01:42 -0500

    replaced jq, all buttons do bare min

--- 13.js
+++ 13.js
@@ -3,18 +3,44 @@ var app = angular.module('modName', []);
 app.controller('theController', function(){
 	var vm = this;
 
-	vm.words = ['love', 'jump', 'serious', 'orange']
+	vm.words = ['love', 'jump', 'serious', 'orange'];
 	
-	vm.randomW = function () {
-		var getIndex=vm.words[Math.floor(Math.random()*vm.words.length)];
-       	$("#display1").append(getIndex + "<br/>");
+	vm.inArray=[];
+	vm.pickWord = function () {
+      var theWord = {};
+      theWord.word = vm.words[Math.floor(Math.random() * vm.words.length)];
+     	vm.inArray.push(theWord);
     };
+
+    vm.worDeco = ['bold'];
+	vm.styledArray=[];
+    vm.wordDecor = function () {
+      var element = {};
+      element.word = vm.words[Math.floor(Math.random() * vm.words.length)];
+      element.worDeco = vm.worDeco[Math.floor(Math.random() * vm.worDeco.length)];
+     	vm.styledArray.push(element);
+    };
+	
+	
 
     vm.phrases = ['A hot potato','A penny for your thoughts','Back to the drawing board','Best thing since sliced bread'];
 	
-	vm.randomP = function () {
-		var getIndex=vm.phrases[Math.floor(Math.random()*vm.phrases.length)];
-       	$("#display2").append(getIndex + "<br/>");
+    vm.phraseArray=[]
+    vm.pickPhrase = function () {
+      var thePhrase = {};
+      thePhrase.phrase = vm.phrases[Math.floor(Math.random() * vm.phrases.length)];
+     	vm.phraseArray.push(thePhrase);
+    };
+
+
+    vm.phraseDeco = ['bold'];
+	vm.styledPhraseArray=[]
+
+    vm.phraseDecor = function () {
+      var phraseElement = {};
+      phraseElement.phrase = vm.phrases[Math.floor(Math.random() * vm.phrases.length)];
+      phraseElement.phraseDeco = vm.phraseDeco[Math.floor(Math.random() * vm.phraseDeco.length)];
+     	vm.styledPhraseArray.push(phraseElement);
     };
 	
 
